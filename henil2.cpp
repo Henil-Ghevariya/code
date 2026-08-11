741fadbec6249305cf76c0a6fcabafe9f92e5a27
Henil-Ghevariya <henilghevariya605@gmail.com>
2026-08-11 17:38:59 +0530

    Add files via upload

---
+++ henil2.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main ()
+{/*program to multiple 2 numbers*/
+  int a,b,c;
+  printf("enter two numbers");
+  scanf("%d%d",&a,&b);  //input
+  c=a*b;  //multiple two numbers
+  printf("%d*%d=%d\n",a,b,c);
+  return 0;
+}
