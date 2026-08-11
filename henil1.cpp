568d37794742f2447fcef59396419dfa85497427
Henil-Ghevariya <henilghevariya605@gmail.com>
2026-08-11 17:24:04 +0530

    Add files via upload

---
+++ henil1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main ()
+{/*program to add 2 numbers*/
+  int a,b,c;
+  printf("enter two numbers");
+  scanf("%d%d",&a,&b);  //input
+  c=a+b;  //add two numbers
+  printf("%d+%d=%d\n",a,b,c);
+  return 0;
+}
