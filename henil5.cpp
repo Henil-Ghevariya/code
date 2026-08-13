9c800f193c3f6d27dd5226e62a0fafa896903058
Henil-Ghevariya <henilghevariya605@gmail.com>
2026-08-13 19:03:05 +0530

    Create henil5.cpp

---
+++ henil5.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main ()
+{/*program to add 2 numbers*/
+  float a,b,c,d,e,f;
+  printf("enter two numbers");
+  scanf("%f%f",&a,&b);  //input
+  c=a+b;  //add two numbers
+  d=a-b;  //subtract two numbers
+  e=a*b;  //multiplication two numbers 
+  f=a/b;  //division two numbers
+  printf("%f+%f=%f\n",a,b,c);
+  printf("%f-%f=%f\n",a,b,d);
+  printf("%f*%f=%f\n",a,b,e);
+  printf("%f/%f=%f\n",a,b,f);
+  return 0;
+}  
