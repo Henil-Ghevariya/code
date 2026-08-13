30ef37d1b43c0d7a4ef63dffb042458515ef39ab
Henil-Ghevariya <henilghevariya605@gmail.com>
2026-08-13 19:46:35 +0530

    Update henil7.cpp

--- henil7.cpp
+++ henil7.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main()
-{
+{ /* converts minutes into hours */
  int a,b;
  printf("enter number of minutes:");
- scanf("%d",&a);
- b=a/60;
- printf("number of hours:%d",b);
+ scanf("%d",&a); //input
+ b=a/60; //transformation 
+ printf("number of hours:%d",b); //output
  return 0;
 }  
