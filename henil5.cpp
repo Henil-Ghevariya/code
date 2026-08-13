579d103bced788fe62de66f9bbeb0049fd8bf53c
Henil-Ghevariya <henilghevariya605@gmail.com>
2026-08-13 19:45:22 +0530

    Update henil5.cpp

--- henil5.cpp
+++ henil5.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main ()
-{/*program to add 2 numbers*/
+{/*program to calculate 2 numbers*/
   float a,b,c,d,e,f;
   printf("enter two numbers");
   scanf("%f%f",&a,&b);  //input
@@ -8,7 +8,7 @@ int main ()
   d=a-b;  //subtract two numbers
   e=a*b;  //multiplication two numbers 
   f=a/b;  //division two numbers
-  printf("%f+%f=%f\n",a,b,c);
+  printf("%f+%f=%f\n",a,b,c); 
   printf("%f-%f=%f\n",a,b,d);
   printf("%f*%f=%f\n",a,b,e);
   printf("%f/%f=%f\n",a,b,f);
