3c893da4e9a83db14680f83f57a1fa857e04c839
Henil-Ghevariya <henilghevariya605@gmail.com>
2026-08-14 19:30:37 +0530

    Create henil19.cpp

---
+++ henil19.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{ /*calculate the area of circle */
+ float a,b;
+ printf("enter the radius of circle:");
+ scanf("%f",&a); // enter the radius
+ b=22.0/7*a*a; // calculate the area
+ printf("the area of circle is %f\n",b); //output area
+ return 0;
+} 
