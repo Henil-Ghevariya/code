df5a59f7c7517cd7dff8d651a029c0a203d60b36
Henil-Ghevariya <henilghevariya605@gmail.com>
2026-08-15 17:53:21 +0530

    Implement average calculation for three subjects
    Fix calculation of average by changing multiplication to addition.

---
+++ henil23.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{/*calculate the average of three subject*/
+ float a,b,c,d;
+ printf("enter the marks of all three subject:");
+ scanf("%f,%f,%f",&a,&b,&c); //input marks
+ d=a*b*c/3; //calculate the average
+ printf("the average of all three subject is %f",d); //
+ return 0;
+}
