ded1837d0d5d5a89766cdca42bf074ade313a838
Henil-Ghevariya <henilghevariya605@gmail.com>
2026-08-15 18:38:09 +0530

    Refactor swap program for better readability
    Updated user prompts and comments for clarity.

--- henil24.cpp
+++ henil24.cpp
@@ -2,11 +2,11 @@
 int main()
 { /*swap two values*/
  int a,b,c;
- printf("enter the two values:");
- scanf("%d %d",&a,&b);
- c=a;
- a=b;
- b=c;
- printf("after swap the values a=%d and b=%d",b,c);
+ printf("enter the two values a and b:");
+ scanf("%d %d",&a,&b); //input a and b
+ c=a; //value of 'a' goes in 'c' and 'a' become empty
+ a=b; //value of 'b' goes in 'a' and 'b' become empty
+ b=c; //value of 'c' goes in 'b' and 'c' become empty
+ printf("after swap the values a=%d and b=%d",a,b); //output
  return 0;
 }
