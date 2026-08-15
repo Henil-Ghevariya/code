7bcaa4da68cb8a19653d2193b2409b2fdf21e6cb
Henil-Ghevariya <henilghevariya605@gmail.com>
2026-08-15 17:57:24 +0530

    Fix input format for subject marks

--- henil23.cpp
+++ henil23.cpp
@@ -3,7 +3,7 @@ int main()
 {/*calculate the average of three subject*/
  float a,b,c,d;
  printf("enter the marks of all three subject:");
- scanf("%f,%f,%f",&a,&b,&c); //input marks
+ scanf("%f %f %f",&a,&b,&c); //input marks
  d=a*b*c/3; //calculate the average
  printf("the average of all three subject is %f",d); //
  return 0;
