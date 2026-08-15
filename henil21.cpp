9baed0963835c057a7442b17759527272675650f
Henil-Ghevariya <henilghevariya605@gmail.com>
2026-08-15 14:15:58 +0530

    Fix typo in scanf function for salary input

--- henil21.cpp
+++ henil21.cpp
@@ -3,7 +3,7 @@ int main()
 { /*calculte net salary*/
   float a,b;
   printf("enter the gross salary:");
-  sacnf("%f",&a);                    //input gross salary
+  scanf("%f",&a);                    //input gross salary
   b=a+a*10/100-a*3/100;              //calculate net salary
   printf("the net salary is %f",b);  //output net salary
   return 0;
