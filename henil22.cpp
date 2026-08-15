47bb18561c838a16a81016a40827728926e7fd56
Henil-Ghevariya <henilghevariya605@gmail.com>
2026-08-15 14:30:58 +0530

    Refactor net sales calculation and improve comments

--- henil22.cpp
+++ henil22.cpp
@@ -3,8 +3,8 @@ int main ()
 { /*calculate the net sales*/
   float a,b;
   printf("enter the gross sales:");
-  scanf("%f",&a);
-  b=a-a*10/100;
-  printf("the net sales is %f",b);
+  scanf("%f",&a); //input gross sales
+  b=a-a*10/100; //calculate net sales
+  printf("the net sales is %f",b); //output net sales
   return 0;
 }
