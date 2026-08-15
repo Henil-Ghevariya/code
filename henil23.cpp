a928a761e737ad16f315fff5ebd07b51a73f3daa
Henil-Ghevariya <henilghevariya605@gmail.com>
2026-08-15 17:59:24 +0530

    Fix average calculation formula in henil23.cpp

--- henil23.cpp
+++ henil23.cpp
@@ -4,7 +4,7 @@ int main()
  float a,b,c,d;
  printf("enter the marks of all three subject:");
  scanf("%f %f %f",&a,&b,&c); //input marks
- d=a*b*c/3; //calculate the average
+ d=(a*b*c)/3; //calculate the average
  printf("the average of all three subject is %f",d); //
  return 0;
 }
