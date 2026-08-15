7dc9d04acbf7de5c50fc2a1f2a7d82a13047c661
Henil-Ghevariya <henilghevariya605@gmail.com>
2026-08-15 14:12:04 +0530

    Implement net salary calculation
    Fix input function typo and improve salary calculation.

---
+++ henil21.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{ /*calculte net salary*/
+  float a,b;
+  printf("enter the gross salary:");
+  sacnf("%f",&a);                    //input gross salary
+  b=a+a*10/100-a*3/100;              //calculate net salary
+  printf("the net salary is %f",b);  //output net salary
+  return 0;
+}
