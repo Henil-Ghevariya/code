87937e0eb3ecedb58a697b125475bba406416592
Henil-Ghevariya <henilghevariya605@gmail.com>
2026-08-15 13:14:27 +0530

    Change data types to float for byte conversion

--- henil13.cpp
+++ henil13.cpp
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main()
 { /* convert bytes in KB,MB and GB*/
- int a,b,c,d;
+ float a,b,c,d;
  printf("enterthe number of bytes:");
- scanf("%d",&a); // input bytes
- b=a*1024; //convert bytes into KB
- c=b*1024; //convert KB into MB
- d=c*1024; //convert MB into GB
- printf("%d bytes=%d KB\n",a,b);
- printf("%d bytes=%d MB\n",a,c);
- printf("%d bytes=%d GB\n",a,d);
+ scanf("%f",&a); // input bytes
+ b=a/1024; //convert bytes into KB
+ c=b/1024; //convert KB into MB
+ d=c/1024; //convert MB into GB
+ printf("%f bytes=%f KB\n",a,b);
+ printf("%f bytes=%f MB\n",a,c);
+ printf("%f bytes=%f GB\n",a,d);
  return 0;
 } 
